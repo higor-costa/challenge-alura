3722c4cf562f43e70af90f91dc680340678429c4
Higor Costa <higorcosta972@gmail.com>
2024-08-09 12:02:14 -0300

    Armazenar elementos do DOM em variáveis

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const caixaTexto = acessaElementosDom('#caixaTexto');
+const botaoCriptografar = acessaElementosDom('#botao-criptografar');
+
 function acessaElementosDom(elemento) {
   return document.querySelector(elemento);
 }
